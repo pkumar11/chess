758c3b46cabc6d44a3abcceec2efd7d4552f6030
studentpankaj <pankaj10101993@gmail.com>
2019-05-21 14:59:05 +0530

    new file is addedd

---
+++ chess.js
@@ -0,0 +1,19 @@
+var c = 1,j;
+for (  var i =1;i <= 8;i++,c++){
+    console.log("\n")
+    j = 1;
+    if ( c% 2==0){
+        while(j<=8){
+            process.stdout.write("*  ");
+            j++;
+        }
+    }
+    else{
+        while(j<=8){
+
+            process.stdout.write("  *");
+            j++;
+        }
+    }
+
+}
